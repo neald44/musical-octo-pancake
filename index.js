797de902edf20ecdc7c17db4c00f00d683c8a8e6
neald44 <neald44@gmail.com>
2024-06-19 16:25:05 -0400

    add projects list menu

---
+++ index.js
@@ -0,0 +1,5 @@
+const projectsMenu = document.getElementById('projects-menu');
+const projectsList = document.getElementById('projects-list');
+
+projectsList.addEventListener('click', () => {
+projectsMenu.classList.toggle('hidden')});
